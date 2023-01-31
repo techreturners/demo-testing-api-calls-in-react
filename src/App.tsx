db3796acf0aebcc5f5a44ca21cf21a5d4c324a14
neil-hughes <neil.hughes@techreturners.com>
2023-01-31 16:38:30 +0000

    fix: Remove dependency on axios; use fetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,40 @@ import Header from './components/header';
 import CharacterContainer from './components/character_container';
 import Navigation from './components/navigation';
 import { DisneyCharacter } from './disney_character';
-import axios from 'axios';
 import { useEffect } from 'react';
 
-export const FavouritesContext = React.createContext<number[]>( []);
+export const FavouritesContext = React.createContext<number[]>([]);
 
-const App : React.FC = () => {
+const App: React.FC = () => {
 
 	const [currentPage, setCurrentPage] = useState<number>(1);
 	const [characterFavourites, setCharacterFavourites] = useState<Array<number>>([]);
 
-  // Some dummy state representing disney characters
-  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
-   
-  ]);
+	// Some dummy state representing disney characters
+	const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
 
-  useEffect(() => {
-	getCharacters(currentPage);
-  }, [currentPage]);
+	]);
 
-  const getCharacters = async (pageNumber : number) => {
-	// Utilised Axios for API calls
-	const apiResponse = await axios.get(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
-	setCharacters(apiResponse.data.data);
-  };
+	useEffect(() => {
+		getCharacters(currentPage);
+	}, [currentPage]);
 
- 
+	const getCharacters = async (pageNumber: number) => {
+		// Utilised Axios for API calls
+		const apiResponse = await fetch(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
+		const json = await apiResponse.json() as { data: DisneyCharacter[] };
+		setCharacters(json.data);
+	};
+
+
 
 	return (
 		<FavouritesContext.Provider value={characterFavourites}>
-		<div className="page">
-			<Header currentPage={currentPage} />
-			<Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
-			<CharacterContainer characters={characters} updateFavourites={setCharacterFavourites}  />
-		</div>
+			<div className="page">
+				<Header currentPage={currentPage} />
+				<Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
+				<CharacterContainer characters={characters} updateFavourites={setCharacterFavourites} />
+			</div>
 		</FavouritesContext.Provider>
 	);
 
