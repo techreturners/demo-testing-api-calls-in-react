404f4afbcf3c3698ab19ee9a7805fdcb4e9257f5
Neil Hughes <94177560+neil-hughes@users.noreply.github.com>
2023-01-31 16:50:50 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,7 @@ const App: React.FC = () => {
 		getCharacters(currentPage);
 	}, [currentPage]);
 
-	const getCharacters = async (pageNumber: number) => {
-		// Utilised Axios for API calls
+	const getCharacters = async (pageNumber: number) => {		
 		const apiResponse = await fetch(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
 		const json = await apiResponse.json() as { data: DisneyCharacter[] };
 		setCharacters(json.data);
